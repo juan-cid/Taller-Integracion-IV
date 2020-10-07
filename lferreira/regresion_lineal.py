7ce7c28c8659b3d9508b7a6d016693aa1c67bdd4
Luis Ferreira <lferreira2017@alu.uct.cl>
2020-10-07 17:49:37 -0300

    Prediccion para 100 dias ( Casos )

--- lferreira/regresion_lineal.py
+++ lferreira/regresion_lineal.py
@@ -17,12 +17,10 @@ for i in range(len(data)):
 # x = dias
 # y = contagios
 
-x_train = np.arange(1,len(data)+1,1)
+n = len(data)+1
+
+x_train = np.arange(1,n,1)
 y_train = np.array(y)
-
-x_nuevo = np.array([10000])
-
-
 
 
 algoritmo = linear_model.LinearRegression()
@@ -31,24 +29,23 @@ algoritmo.fit(x_train.reshape(-1,1),y_train)
 
 print('w = ' + str(algoritmo.coef_) + ', b = ' + str(algoritmo.intercept_))
 
-#Se genera la prediccion
-
-prediccion = algoritmo.predict(x_nuevo.reshape(-1,1))
-print(prediccion)
 
 
+
+# se agregan 100 dias para realizar prediccion
+x_nuevo = np.arange(1,n + 100)
+
+# Predecimos los valores y para los datos usados en el entrenamiento
+predic = algoritmo.predict(x_nuevo.reshape(-1,1))
 
 #Error Cuadratico medio
 
-# Predecimos los valores y para los datos usados en el entrenamiento
-prediccion_entrenamiento = algoritmo.predict(x_train.reshape(-1,1))
-
 # Calculamos el Error Cuadrático Medio (MSE = Mean Squared Error)
-mse = mean_squared_error(y_true = y_train, y_pred = prediccion_entrenamiento)
+#mse = mean_squared_error(y_true = y_train, y_pred = prediccion_entrenamiento)
 # La raíz cuadrada del MSE es el RMSE
-rmse = np.sqrt(mse)
-print('Error Cuadrático Medio (MSE) = ' + str(mse))
-print('Raíz del Error Cuadrático Medio (RMSE) = ' + str(rmse))
+#rmse = np.sqrt(mse)
+#print('Error Cuadrático Medio (MSE) = ' + str(mse))
+#print('Raíz del Error Cuadrático Medio (RMSE) = ' + str(rmse))
 
 
 # calculamos el coeficiente de determinación R2
@@ -58,10 +55,18 @@ print('Coeficiente de Determinación R2 = ' + str(r2))
 
 
 
+#Precision del modelo 
+print(algoritmo.score(x_train.reshape(-1,1),y_train))
+
 
 
 #Graficar los datos
+plt.title("Prediccion datos para n + 100 \n ( n =  Dias , cantidad dias datos oficiales 220)")
+plt.ylabel("Casos")
+plt.xlabel("Dias")
 
-plt.scatter(x_train,y_train,label='Data',color='blue')
-plt.plot(x_train,prediccion_entrenamiento,color='red',label="Regresion lineal")
+
+plt.scatter(x_train,y_train,label='Datos Oficiales',color='blue')
+plt.plot(predic,color='red',label="Regresion lineal")
+plt.legend()
 plt.show()
